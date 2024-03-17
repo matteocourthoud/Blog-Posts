7cca1a9a4bea1cc854c1ea3b23040e5664179102
matteocourthoud <matteo.courthoud@uzh.ch>
2024-03-17 17:19:45 +0100

    clustering

--- notebooks/src/dgp.py
+++ notebooks/src/dgp.py
@@ -69,26 +69,26 @@ class DGP:
         """Post-treatment processing."""
         return df
 
-    def generate_data(self, seed_data=0, seed_assignment=1, drop_unobservables: bool = True, **kwargs) -> pd.DataFrame:
+    def generate_data(self, seed_dt=0, seed_po=1, seed_as=2, seed_pt=3, drop_unobservables: bool = True, **kwargs) -> pd.DataFrame:
         """Generate potential outcomes, add assignment and select realized outcomes."""
-        df = self.initialize_data(seed=seed_data)
-        df = self.add_potential_outcomes(df=df, seed=seed_data, **kwargs)
-        df = self.add_treatment_assignment(df=df, seed=seed_assignment)
+        df = self.initialize_data(seed=seed_dt)
+        df = self.add_potential_outcomes(df=df, seed=seed_po, **kwargs)
+        df = self.add_treatment_assignment(df=df, seed=seed_as)
         self.check_potential_outcomes(df=df)
         df = self.add_realized_outcomes(df, drop_unobservables=drop_unobservables)
-        return self.post_treatment_processing(df=df, seed=seed_data)
+        return self.post_treatment_processing(df=df, seed=seed_pt)
 
     def evaluate_f_redrawing_data(self, f, n_draws: int):
-        """Evaluates the function f on n_draws of the data (both potential outcomes and treatment assignment)."""
-        results = Parallel(n_jobs=8)(delayed(f)(self.generate_data(seed_data=i, seed_assignment=n_draws+i)) for i in range(n_draws))
+        """Evaluates the function f on n_draws of the data (data, potential outcomes, and treatment assignment)."""
+        results = Parallel(n_jobs=8)(delayed(f)(self.generate_data(seed_dt=i, seed_po=n_draws+1, seed_as=2*n_draws+i)) for i in range(n_draws))
         return results
 
-    def evaluate_f_redrawing_assignment(self, f, n_draws: int):
-        """Evaluates the function f on n_draws of the treatment assignment (not the potential outcomes)."""
-        results = Parallel(n_jobs=8)(delayed(f)(self.generate_data(seed_assignment=i)) for i in range(n_draws))
+    def evaluate_f_redrawing_potential_outcomes(self, f, n_draws: int):
+        """Evaluates the function f on n_draws of the potential outcomes, and treatment assignment (not the data)."""
+        results = Parallel(n_jobs=8)(delayed(f)(self.generate_data(seed_po=n_draws+1, seed_as=2*n_draws+i)) for i in range(n_draws))
         return results
     
-    def evaluate_f_redrawing_potential_outcomes(self, f, n_draws: int):
-        """Evaluates the function f on n_draws of the potential outcomes (not the assignment)."""
-        results = Parallel(n_jobs=8)(delayed(f)(self.generate_data(seed_data=i)) for i in range(n_draws))
+    def evaluate_f_redrawing_assignment(self, f, n_draws: int):
+        """Evaluates the function f on n_draws of the treatment assignment (not the data, or the potential outcomes)."""
+        results = Parallel(n_jobs=8)(delayed(f)(self.generate_data(seed_as=2*n_draws+i)) for i in range(n_draws))
         return results
